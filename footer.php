f60e56769aefb7f1c151198b1ed780a2a0de2d85
FernandoRuar-SpaceUnite <fernandoruarjr@gmail.com>
2016-11-22 09:32:24 +0800

    contact_me

---
+++ footer.php
@@ -0,0 +1,15 @@
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Your Website 2016</p>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+    <!-- /.container -->
+<?php wp_footer(); ?>
+</body>
+
+</html>
